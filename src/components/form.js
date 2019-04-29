1b8c67f68e8a4baabeaa49682be27d47aee9c878
wang0805 <wh_wang@live.com>
2019-04-29 16:38:20 +0800

    added Q and split csv n recap

--- src/components/form.js
+++ src/components/form.js
@@ -32,6 +32,8 @@ class Form extends Component {
     s_client_id: "",
     b_trader: [],
     s_trader: [],
+    b_broker: "",
+    s_broker: "",
     b_accounts: [],
     s_accounts: [],
     s_comms: 0.0,
@@ -76,7 +78,9 @@ class Form extends Component {
     let exec_date = date.getFullYear() + "-" + month + "-" + day;
     this.setState({ execDate: exec_date });
     const options = { hour12: false };
-    this.setState({ execTime: date.toLocaleTimeString('en-US', options).substring(0, 5) });
+    this.setState({
+      execTime: date.toLocaleTimeString("en-US", options).substring(0, 5)
+    });
     try {
       fetch("/api/clients", {
         method: "GET",
@@ -86,7 +90,6 @@ class Form extends Component {
       })
         .then(res => res.json())
         .then(data => {
-          console.log(data, "dataaaaaaaa")
           let clients = [" "];
           let clientsObj = [];
           for (let i = 0; i < data.length; i++) {
@@ -103,7 +106,7 @@ class Form extends Component {
 
             for (let j = 0; j < data.length; j++) {
               if (data[j].client_name === clients[i]) {
-                id = data[j].id
+                id = data[j].id;
                 traders.push(data[j].trader_name);
                 accounts.push(data[j].account);
                 recap_emails = data[j].recap_emails;
@@ -137,7 +140,6 @@ class Form extends Component {
     } catch (e) {
       console.log(e, "error getting clients due to permissions");
     }
-   
   }
 
   handleChange = e => {
@@ -232,6 +234,8 @@ class Form extends Component {
     let s_trader = this.state.s_trader;
     let s_accounts = this.state.s_accounts;
     let s_comms = this.state.s_comms;
+    let s_broker = this.state.s_broker;
+    let b_broker = this.state.b_broker;
 
     this.setState({
       b_client: s_client,
@@ -241,7 +245,9 @@ class Form extends Component {
       s_client: b_client,
       s_trader: b_trader,
       s_accounts: b_accounts,
-      s_comms: b_comms
+      s_comms: b_comms,
+      s_broker: b_broker,
+      b_broker: s_broker
     });
   };
 
@@ -254,13 +260,30 @@ class Form extends Component {
       s_client: "",
       s_trader: "",
       s_accounts: "",
-      s_comms: 0
+      s_comms: 0,
+      s_broker: "",
+      b_broker: ""
     });
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
+  handleQ1 = () => {
+    this.setState({ fromM: "Jan" });
+    this.setState({ toM: "Mar" });
+  };
+  handleQ2 = () => {
+    this.setState({ fromM: "Apr" });
+    this.setState({ toM: "Jun" });
+  };
+  handleQ3 = () => {
+    this.setState({ fromM: "Jul" });
+    this.setState({ toM: "Sep" });
+  };
+  handleQ4 = () => {
+    this.setState({ fromM: "Oct" });
+    this.setState({ toM: "Dec" });
+  };
 
+  handleCsv = () => {
     let toM = this.getMon(this.state.toM);
     let fromM = this.getMon(this.state.fromM);
     let consMonth = toM - fromM + 1;
@@ -277,22 +300,10 @@ class Form extends Component {
       gcmB = this.state.b_accounts.split(" ")[1];
     }
     console.log(fromM, toM, consMonth, "checking to and from");
-    let contract = "";
-    if (fromM === 1 && consMonth === 3) {
-      contract = "Q1'" + this.state.year;
-    } else if (fromM === 4 && consMonth === 3) {
-      contract = "Q2'" + this.state.year;
-    } else if (fromM === 7 && consMonth === 3) {
-      contract = "Q3'" + this.state.year;
-    } else if (fromM === 10 && consMonth === 3) {
-      contract = "Q4'" + this.state.year;
-    } else {
-      contract = this.state.fromM + "'" + this.state.year;
-    }
 
     let oet = "NLT";
-    if(this.state.instrument === "S"){
-      oet = "OTC"
+    if (this.state.instrument === "S") {
+      oet = "OTC";
     }
 
     const rows = [
@@ -345,6 +356,100 @@ class Form extends Component {
         ""
       ]
     ];
+
+    let csvContent =
+      "data:text/csv;charset=utf-8," + rows.map(e => e.join(",")).join("\n");
+    var encodedUri = encodeURI(csvContent);
+    window.open(encodedUri);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    let toM = this.getMon(this.state.toM);
+    let fromM = this.getMon(this.state.fromM);
+    let consMonth = toM - fromM + 1;
+    // let date = this.state.execDate.split("-");
+    // let execDate = date[2] + "/" + date[1] + "/" + date[0];
+    // let gcmB = "";
+    // let gcmS = "";
+    // if (this.state.b_accounts.length > 0 && this.state.s_accounts.length > 0) {
+    //   gcmB = this.state.b_accounts.split(" ")[1];
+    //   gcmS = this.state.s_accounts.split(" ")[1];
+    // } else if (this.state.s_accounts.length > 0) {
+    //   gcmS = this.state.s_accounts.split(" ")[1];
+    // } else if (this.state.b_accounts.length > 0) {
+    //   gcmB = this.state.b_accounts.split(" ")[1];
+    // }
+    console.log(fromM, toM, consMonth, "checking to and from");
+    let contract = "";
+    if (fromM === 1 && consMonth === 3) {
+      contract = "Q1'" + this.state.year;
+    } else if (fromM === 4 && consMonth === 3) {
+      contract = "Q2'" + this.state.year;
+    } else if (fromM === 7 && consMonth === 3) {
+      contract = "Q3'" + this.state.year;
+    } else if (fromM === 10 && consMonth === 3) {
+      contract = "Q4'" + this.state.year;
+    } else {
+      contract = this.state.fromM + "'" + this.state.year;
+    }
+
+    // let oet = "NLT";
+    // if (this.state.instrument === "S") {
+    //   oet = "OTC";
+    // }
+
+    // const rows = [
+    //   [
+    //     "Group No.",
+    //     "Trade type",
+    //     "Leg Group",
+    //     "Exec Date",
+    //     "Exec Time",
+    //     "Product",
+    //     "Month/Year",
+    //     "Instrument Type",
+    //     "Off-exchange Type",
+    //     "Strike Px",
+    //     "Cons Months",
+    //     "B/S",
+    //     "Qty",
+    //     "Price",
+    //     "Party1 IDB",
+    //     "Party1 GCM",
+    //     "Party1 A/C",
+    //     "Party1 Comment",
+    //     "Party2 IDB",
+    //     "Party2 GCM",
+    //     "Party2 A/C",
+    //     "Party2 Comment"
+    //   ],
+    //   [
+    //     this.state.dealGroup,
+    //     "Outright",
+    //     "1",
+    //     execDate,
+    //     this.state.execTime,
+    //     this.state.product_code,
+    //     fromM.toString() + "/" + this.state.year,
+    //     this.state.instrument,
+    //     oet,
+    //     this.state.strike,
+    //     consMonth,
+    //     "S",
+    //     this.state.qty,
+    //     this.state.price,
+    //     this.state.s_idb,
+    //     gcmS,
+    //     this.state.s_accounts,
+    //     "",
+    //     this.state.b_idb,
+    //     gcmB,
+    //     this.state.b_accounts,
+    //     ""
+    //   ]
+    // ];
     if (
       window.confirm(
         `Please check the below info:
@@ -369,10 +474,10 @@ class Form extends Component {
       )
     ) {
       //download to csv
-      let csvContent =
-        "data:text/csv;charset=utf-8," + rows.map(e => e.join(",")).join("\n");
-      var encodedUri = encodeURI(csvContent);
-      window.open(encodedUri);
+      // let csvContent =
+      //   "data:text/csv;charset=utf-8," + rows.map(e => e.join(",")).join("\n");
+      // var encodedUri = encodeURI(csvContent);
+      // window.open(encodedUri);
 
       // console.log(this.state, "states passing through");
       const data = { ...this.state, contract: contract };
@@ -402,7 +507,6 @@ class Form extends Component {
   };
 
   render() {
-    console.log(this.state.value, "testing values to see")
     const { classes } = this.props;
     let b_traders = [];
     let b_accounts = [];
@@ -468,6 +572,25 @@ class Form extends Component {
             name="execTime"
             type="time"
             value={this.state.execTime}
+            onChange={this.handleChange}
+            variant="outlined"
+          />
+          <span>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </span>
+          <TextField
+            className={classes.textControl}
+            label="Buy Broker"
+            name="b_broker"
+            value={this.state.b_broker}
+            onChange={this.handleChange}
+            variant="outlined"
+          />
+          <TextField
+            className={classes.textControl}
+            label="Sell Broker"
+            name="s_broker"
+            value={this.state.s_broker}
             onChange={this.handleChange}
             variant="outlined"
           />
@@ -662,7 +785,9 @@ class Form extends Component {
             </Select>
           </FormControl>
           {year}
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+          <span>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </span>
           <FormControl className={classes.dateControl} variant="outlined">
             <InputLabel
               ref={ref => {
@@ -697,6 +822,23 @@ class Form extends Component {
             </Select>
           </FormControl>
           {year}
+          <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+          <Button variant="contained" color="default" onClick={this.handleQ1}>
+            Q1
+          </Button>
+          <span>&nbsp;&nbsp;</span>
+          <Button variant="contained" color="default" onClick={this.handleQ2}>
+            Q2
+          </Button>
+          <span>&nbsp;&nbsp;</span>
+          <Button variant="contained" color="default" onClick={this.handleQ3}>
+            Q3
+          </Button>
+          <span>&nbsp;&nbsp;</span>
+          <Button variant="contained" color="default" onClick={this.handleQ4}>
+            Q4
+          </Button>
+
           <br />
           <br />
           <FormControl className={classes.formControl} variant="outlined">
@@ -828,10 +970,15 @@ class Form extends Component {
           <br />
           <br />
           <Button type="submit" variant="contained" color="primary">
-            Submit
+            Submit Recap
+          </Button>
+          <span>&nbsp;&nbsp;</span>
+          <Button onClick={this.handleCsv} variant="contained" color="primary">
+            CSV
           </Button>
         </form>
         <br />
+        <span>&nbsp;&nbsp;</span>
         <Button onClick={this.flip} variant="contained" color="default">
           Flip
         </Button>
