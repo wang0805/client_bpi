2aa6caea8df3e9ab1570ac59cf8b6158f0b5017b
wang0805 <wh_wang@live.com>
2019-05-24 12:16:27 +0800

    transactions show by created_by date

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -90,78 +90,87 @@ class Transactions extends Component {
             {this.state.data.length > 0 &&
               this.state.data.map(row => {
                 let date = new Date(row.created_at);
+                let now = new Date();
                 let date_time =
                   date.toLocaleDateString() + " " + date.toLocaleTimeString();
                 let trade_date = new Date(row.trade_date).toLocaleDateString();
-                return (
-                  <TableRow key={row.trade_id}>
-                    <CustomTableCell align="center" component="th" scope="row">
-                      {row.trade_id}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {trade_date}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.product}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.instrument}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.b_client}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.b_account}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.b_trader}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.b_commission}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.s_client}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.s_account}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.s_trader}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.s_commission}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.strike}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.price}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">{row.qty}</CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.contract}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.deal_id}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {row.created_by}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      {date_time}
-                    </CustomTableCell>
-                    <CustomTableCell align="center">
-                      <Button
-                        size="small"
-                        variant="outlined"
-                        color="primary"
-                        onClick={() => this.updatePost(row.trade_id)}
+                if (date.getDate() === now.getDate()) {
+                  return (
+                    <TableRow key={row.trade_id}>
+                      <CustomTableCell
+                        align="center"
+                        component="th"
+                        scope="row"
                       >
-                        Edit
-                      </Button>
-                    </CustomTableCell>
-                  </TableRow>
-                );
+                        {row.trade_id}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {trade_date}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.product}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.instrument}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.b_client}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.b_account}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.b_trader}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.b_commission}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.s_client}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.s_account}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.s_trader}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.s_commission}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.strike}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.price}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.qty}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.contract}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.deal_id}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {row.created_by}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        {date_time}
+                      </CustomTableCell>
+                      <CustomTableCell align="center">
+                        <Button
+                          size="small"
+                          variant="outlined"
+                          color="primary"
+                          onClick={() => this.updatePost(row.trade_id)}
+                        >
+                          Edit
+                        </Button>
+                      </CustomTableCell>
+                    </TableRow>
+                  );
+                }
               })}
           </TableBody>
         </Table>
